fc413eaca2301ccbb7c169695c56abfe0290a6e8
siasimeng <55194373+siasimeng@users.noreply.github.com>
2022-01-08 02:59:16 +0800

    Update preprocessing.py

--- app/processors/preprocessing.py
+++ app/processors/preprocessing.py
@@ -1,5 +1,6 @@
 import nltk
 nltk.download('stopwords')
+nltk.download('punkt')
 from nltk.tokenize import word_tokenize, RegexpTokenizer # tokenize words
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
