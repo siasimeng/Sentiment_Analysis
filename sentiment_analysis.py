e0c3301bd355ae4c5d7dd61ee514ac865218bc66
siasimeng <55194373+siasimeng@users.noreply.github.com>
2022-01-08 08:37:10 +0800

    Update sentiment_analysis.py

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -21,5 +21,26 @@ def main():
             return render_template('main.html', error=e)
     return render_template('main.html', error=None)
 
+@app.route('/plot')
+def plot():
+    left = [1, 2, 3, 4, 5]
+    # heights of bars
+    height = [10, 24, 36, 40, 5]
+    # labels for bars
+    tick_label = ['one', 'two', 'three', 'four', 'five']
+    # plotting a bar chart
+    plt.bar(left, height, tick_label=tick_label, width=0.8, color=['red', 'green'])
+
+    # naming the y-axis
+    plt.ylabel('y - axis')
+    # naming the x-axis
+    plt.xlabel('x - axis')
+    # plot title
+    plt.title('My bar chart!')
+
+    plt.savefig('static/images/plot.png')
+
+    return render_template('plot.html', url='/static/images/plot.png')
+
 if __name__ == '__main__':
     app.run()
