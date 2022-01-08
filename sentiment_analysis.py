79f53eeb739f0440881cea3362adc5d630c394f2
siasimeng <55194373+siasimeng@users.noreply.github.com>
2022-01-08 08:43:15 +0800

    Update sentiment_analysis.py

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -15,32 +15,27 @@ def main():
         try:
             sp = SentimentApp(url)
             result = sp.run()
-            return render_template('result.html', result=result, error=None)
+            left = [1, 2, 3, 4, 5]
+            # heights of bars
+            height = [10, 24, 36, 40, 5]
+            # labels for bars
+            tick_label = ['one', 'two', 'three', 'four', 'five']
+            # plotting a bar chart
+            plt.bar(left, height, tick_label=tick_label, width=0.8, color=['red', 'green'])
+
+            # naming the y-axis
+            plt.ylabel('y - axis')
+            # naming the x-axis
+            plt.xlabel('x - axis')
+            # plot title
+            plt.title('My bar chart!')
+
+            plt.savefig('static/images/plot.png')
+            return render_template('result.html', result=result, url='/plot.png', error=None)
         except Exception as e:
             print(e)
             return render_template('main.html', error=e)
     return render_template('main.html', error=None)
-
-@app.route('/plot')
-def plot():
-    left = [1, 2, 3, 4, 5]
-    # heights of bars
-    height = [10, 24, 36, 40, 5]
-    # labels for bars
-    tick_label = ['one', 'two', 'three', 'four', 'five']
-    # plotting a bar chart
-    plt.bar(left, height, tick_label=tick_label, width=0.8, color=['red', 'green'])
-
-    # naming the y-axis
-    plt.ylabel('y - axis')
-    # naming the x-axis
-    plt.xlabel('x - axis')
-    # plot title
-    plt.title('My bar chart!')
-
-    plt.savefig('static/images/plot.png')
-
-    return render_template('result.html', url='/plot.png')
 
 if __name__ == '__main__':
     app.run()
