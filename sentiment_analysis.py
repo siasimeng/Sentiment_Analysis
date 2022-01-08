a5df7a61e44e2bfffbde487ef12fe468dec99a0d
siasimeng <55194373+siasimeng@users.noreply.github.com>
2022-01-08 09:34:12 +0800

    Update sentiment_analysis.py

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, request
 from app.sentiment_app import SentimentApp
 from config import Config
 from io import BytesIO
+import base64
 import matplotlib.pyplot as plt
 
 
