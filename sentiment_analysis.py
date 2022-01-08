2144804bc59aa38d8ce86dd7c41f346632e27f4b
siasimeng <55194373+siasimeng@users.noreply.github.com>
2022-01-08 11:16:51 +0800

    Update sentiment_analysis.py

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -19,20 +19,19 @@ def main():
             sp = SentimentApp(url)
             result = sp.run()
             img = BytesIO()
-            left = [1, 2, 3, 4, 5]
-            # heights of bars
-            height = [10, 24, 36, 40, 5]
-            # labels for bars
-            tick_label = ['one', 'two', 'three', 'four', 'five']
-            # plotting a bar chart
-            plt.bar(left, height, tick_label=tick_label, width=0.8, color=['red', 'green'])
+            
+            positive = result.positive
+            negative = result.negative
 
-            # naming the y-axis
-            plt.ylabel('y - axis')
-            # naming the x-axis
-            plt.xlabel('x - axis')
-            # plot title
-            plt.title('My bar chart!')
+            labels = ['Positive','Negative']
+            values = np.array([positive,negative])
+            myexplode = [0.1, 0]
+            mycolors = ["blue", "red"]
+
+            fig,ax = plt.subplots(figsize=(6,5))
+            ax.pie(values, labels = labels, explode = myexplode, shadow = True, colors = mycolors)
+            ax.legend()
+            ax.set_title("Positive vs Negative Text(%)")
 
             plt.savefig(img, format='png')
             plt.close()
